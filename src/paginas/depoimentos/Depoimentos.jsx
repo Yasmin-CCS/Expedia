740289b496a457737e627b9df2ca86ea480c6ece
Yasmin <yas.carozzi@gmail.com>
2023-09-27 21:38:56 -0300

    Depoimentos

--- src/paginas/depoimentos/Depoimentos.jsx
+++ src/paginas/depoimentos/Depoimentos.jsx
@@ -1,4 +1,5 @@
 import './Depoimentos.css'
+import Button from '../../components/Button'
 
 function Depoimentos() {
 
@@ -6,8 +7,23 @@ function Depoimentos() {
     <>
       <div className='depoimentosPage'>
         <h1>Depoimentos</h1>
-        <div className='boxTexto'>
-          <h4>O que você vai aprender:</h4>
+        <div className='contentDepoimentos'>
+          <div>
+        <div className='boxTexto boxDepoimentos'>
+          <h5>De ontem para hoje, já vendi 2 cursos!</h5><h5> Que dica incrível e simples que você deu!</h5>
+        </div>
+        <div className="buttonSobre desktop">
+            <Button />
+        </div>
+        </div>
+        <div className='boxTexto boxDepoimentos'>
+          <h5>Eu amo o seu trabalho como social media. Cheguei com a ideia e você deu forma a tudo, 
+            é incrível! </h5><h5>A maneira como você estrutura a agenda de conteúdo é excelente. Sua criatividade 
+            é única, amei!</h5>
+        </div>
+        <div className="buttonSobre mobile">
+            <Button />
+        </div>
         </div>
       </div>
     </>
